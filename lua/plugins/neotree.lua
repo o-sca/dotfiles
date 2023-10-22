0e0a43922aa9ae8e1cfb8971298e6ac1b5302c4a
oscar <97479266+o-sca@users.noreply.github.com>
2023-10-22 15:08:13 -0700

    refactor(neotree): update config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      width = 30,
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
+}
