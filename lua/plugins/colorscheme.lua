594ac3838bb0e956d095a221addd9247606148ec
oscar <97479266+o-sca@users.noreply.github.com>
2023-10-22 14:54:00 -0700

    feat: oxocarbon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,40 @@
 return {
+  -- {
+  -- "folke/tokyonight.nvim",
+  -- opts = {
+  --   name = "tokyonight",
+  --   style = "night",
+  --   transparent = true,
+  --   styles = {
+  --     sidebars = "transparent",
+  --     floats = "dark",
+  --     comments = { italic = true },
+  --     keywords = { italic = true, bold = true },
+  --     functions = { bold = true },
+  --     variables = {},
+  --   },
+  --   on_highlights = function(hl, _)
+  --     hl.MatchParen = { bg = "#ff9e64" }
+  --     hl.Type = { fg = "#2ac3de", bold = true, italic = true }
+  --     hl["@keyword"] = {
+  --       fg = "#9d7cd8",
+  --       style = {},
+  --     }
+  --     hl["@keyword.function"] = {
+  --       fg = "#bb9af7",
+  --       style = {},
+  --     }
+  --   end,
+  -- },
+  -- },
   {
-    "folke/tokyonight.nvim",
+    "nyoom-engineering/oxocarbon.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
     opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "dark",
-        comments = { italic = true },
-        keywords = { italic = true, bold = true },
-        functions = { bold = true },
-        variables = {},
-      },
-      on_highlights = function(hl, _)
-        hl.MatchParen = { bg = "#ff9e64" }
-        hl.Type = { fg = "#2ac3de", bold = true, italic = true }
-        hl["@keyword"] = {
-          fg = "#9d7cd8",
-          style = {},
-        }
-        hl["@keyword.function"] = {
-          fg = "#bb9af7",
-          style = {},
-        }
-      end,
+      colorscheme = "oxocarbon",
     },
   },
 }
