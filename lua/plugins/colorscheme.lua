5d646cc22d7dfda73d7e1e10a56af188e997c329
oscar <97479266+o-sca@users.noreply.github.com>
2023-11-05 11:18:19 -0800

    feat(colorscheme): nightfox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,77 @@
 return {
+  {
+    "EdenEast/nightfox.nvim",
+    opts = {
+      options = {
+        transparent = true,
+        styles = {
+          comments = "italic", -- value is any valid attr-list value `:help attr-list`
+          conditionals = "NONE",
+          constants = "NONE",
+          functions = "bold",
+          keywords = "bold, italic",
+          numbers = "NONE",
+          operators = "NONE",
+          strings = "NONE",
+          types = "bold, italic",
+          variables = "NONE",
+        },
+        inverse = { -- Inverse highlight for different types
+          match_paren = true,
+          visual = false,
+          search = false,
+        },
+      },
+    },
+  },
   -- {
-  -- "folke/tokyonight.nvim",
-  -- opts = {
-  --   name = "tokyonight",
-  --   style = "night",
-  --   transparent = true,
-  --   styles = {
-  --     sidebars = "transparent",
-  --     floats = "dark",
-  --     comments = { italic = true },
-  --     keywords = { italic = true, bold = true },
-  --     functions = { bold = true },
-  --     variables = {},
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     name = "tokyonight",
+  --     style = "night",
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "dark",
+  --       comments = { italic = true },
+  --       keywords = { italic = true, bold = true },
+  --       functions = { bold = true },
+  --       variables = {},
+  --     },
+  --     on_highlights = function(hl, _)
+  --       hl.MatchParen = { bg = "#ff9e64" }
+  --       hl.Type = { fg = "#2ac3de", bold = true, italic = true }
+  --       hl["@keyword"] = {
+  --         fg = "#9d7cd8",
+  --         style = {},
+  --       }
+  --       hl["@keyword.function"] = {
+  --         fg = "#bb9af7",
+  --         style = {},
+  --       }
+  --     end,
   --   },
-  --   on_highlights = function(hl, _)
-  --     hl.MatchParen = { bg = "#ff9e64" }
-  --     hl.Type = { fg = "#2ac3de", bold = true, italic = true }
-  --     hl["@keyword"] = {
-  --       fg = "#9d7cd8",
-  --       style = {},
-  --     }
-  --     hl["@keyword.function"] = {
-  --       fg = "#bb9af7",
-  --       style = {},
-  --     }
+  -- },
+  -- {
+  --   "nyoom-engineering/oxocarbon.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd("colorscheme oxocarbon")
+  --     vim.api.nvim_set_hl(0, "CursorColumn", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "Normal", { bg = "none", fg = "none" })
+  --     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", fg = "none" })
+  --     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none", fg = "none" })
+  --     vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+  --     vim.api.nvim_set_hl(0, "Function", { bold = true })
+  --     vim.api.nvim_set_hl(0, "Type", { bold = true, italic = true })
   --   end,
   -- },
-  -- },
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-    priority = 1000,
-  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "oxocarbon",
+      colorscheme = "carbonfox",
     },
   },
 }
