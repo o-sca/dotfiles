a2c74eea5a810cc0869425d7b5fe613242b6aef6
oscar <97479266+o-sca@users.noreply.github.com>
2024-05-17 18:42:16 -0700

    fix(indent-blankline): revert to previous commit
    https://github.com/lukas-reineke/indent-blankline.nvim/issues/876

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,6 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  commit = "3d08501caef2329aba5121b753e903904088f7e6", -- release v3.5.4
+  dependencies = "nvim-treesitter/nvim-treesitter",
+}
